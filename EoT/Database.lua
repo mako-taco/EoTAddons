43febf39c2ffda09a16ebfd0bff1d8412f3f08f8
colinsoleim <soleimc@gmail.com>
2019-08-11 17:48:28 -0500

    Adds basic UI to process items from mailbox

--- EoT/Database.lua
+++ EoT/Database.lua
@@ -3,7 +3,22 @@ EpochTools.db = EpochTools:NewModule("DB")
 function EpochTools.db:SetDefaults()
   EpochToolsDB = {
     Donations = { }, 
-    Prices = { } 
+    Prices = { }, 
+    char = {
+      altDel = false,
+      ctrlRet = true,
+      shiftTake = true,
+      takeAll = true,
+      inboxUI = true,
+      takeStackable = true,
+      sortField = 1,
+    },
+    profile = {
+      disableTooltips = false,
+      scale = 1.0, 
+      font = "Friz Quadrata TT",
+      fontSize = 12
+    },
   }
 
   -- Set Spreadsheet Headers
@@ -14,10 +29,12 @@ function EpochTools.db:SetDefaults()
     ItemValue = "Value"
   }
 
-  -- Set Minor Health Potion for testing
+  -- Set Elixir of the Mongoose for testing
   local price = {
-    ItemID = 118,
-    ItemValue = "30"
+    id = 13452,
+    name = "Elixir of the Mongoose",
+    gold = 2.5,
+    date = 1565256633
   }
   
   table.insert(EpochToolsDB.Donations, donation)
