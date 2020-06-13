1faa38c7e272cf777904b1c4a741d8bbc54ca725
Jake Scott <jake.y.scott@gmail.com>
2020-06-13 13:47:33 -0700

    Update GodGroup.lua

--- EoTPTR/commands/group/GodGroup.lua
+++ EoTPTR/commands/group/GodGroup.lua
@@ -1,7 +1,7 @@
 function EOT_GodGroup(toggle)
   EOT_ForEachGroupMember(
     function (name)
-      EOT_RunCommand("god", toggle, name)
+      EOT_RunCommand("cheat god", toggle, name)
     end
   )
 end
